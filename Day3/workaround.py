b9413608f05b80d99b5f774c15ef9d324a028f56
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-06-25 11:24:03 +0530

    Add files via upload

---
+++ Day3/workaround.py
@@ -0,0 +1,25 @@
+# Getters And Setters
+
+class Student:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+        
+    def get_name(self): #getter
+        return self.__name
+    
+    def get_age(self):
+        return self.__age
+    
+    def set_name(self, name): # setter
+        self.__name = name
+
+n = Student("Arjun", 20)
+
+print(n.get_name())
+
+print(n.get_age())
+
+n.set_name("Nagarjun")
+
+print(n.get_name())
