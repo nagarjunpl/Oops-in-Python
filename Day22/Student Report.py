14496c8969216e9fabd9ab92fa753b25fd1ea380
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-06-29 12:21:56 +0530

    Update Student Report.py

--- Day22/Student Report.py
+++ Day22/Student Report.py
@@ -12,4 +12,51 @@ class Student:
     def add_marks(self, subject, marks):
         self.__marks[subject] = marks
 
-    
+    def calculate_average(self):
+        total_marks = 0
+        for marks in self.__marks.values():
+            total_marks += marks
+        average = total_marks / len(self.__marks)
+        print(f"Average marks for {self.name} (Roll No: {self.roll_no}) : {average}")
+        return average
+
+    def is_passed(self):
+        has_failed = any(marks<35 for marks in self.__marks.values())
+        if has_failed:
+            print(f"{self.name} (Roll No: {self.roll_no}) has failed.")
+        else:
+            print(f"{self.name} (Roll No: {self.roll_no}) has passed.")
+
+    def calculate_grade(self):
+        print("Grade : ", end="")
+        average = self.calculate_average()*100
+        if  average >= 90:
+            print("Grade: A")
+        elif average >= 70:
+            print("Grade: B")
+        elif average >= 50:
+            print("Grade: C")
+        else:
+            print("Grade: F")
+
+class StudentReport:
+    @staticmethod
+    def generate(student: Student):
+        student_marks = student.get_marks()
+        print(f"Name : {student.name} \t Roll No : {student.roll_no}")
+
+        print("-----Marks-----")
+        for subject, marks in student_marks.items():
+            print(f"{subject} : {marks}")
+            
+        print("----------------")
+
+        student.is_passed()
+        student.calculate_grade()
+
+a =  Student(1, "Arjun")
+a.add_marks("Math", 97)
+a.add_marks("Science", 96)
+a.add_marks("English", 34) 
+
+StudentReport.generate(a)
