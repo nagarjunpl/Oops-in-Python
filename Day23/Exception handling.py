4667b6ec84fdeba538d41e7e157ee0cec3a64363
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-07-01 08:51:24 +0530

    Update Exception handling.py

--- Day23/Exception handling.py
+++ Day23/Exception handling.py
@@ -29,3 +29,18 @@ finally:
     print("This block always executes, regardless of an error.")
 
 
+# Exception handling with else and finally blocks
+a = int(input("Enter A value: "))
+b = input("Enter B value: ")
+
+try:
+    print(a/b)
+
+except ValueError as e:
+    print("An error occurred:", e)
+
+else:
+    print("No errors occurred.") 
+
+finally:
+    print("This block always executes, regardless of an error.")
