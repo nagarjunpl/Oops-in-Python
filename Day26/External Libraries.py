d8657dcc6fe8478aed3645eab399472f95f1035e
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-07-12 19:32:30 +0530

    Update External Libraries.py

--- Day26/External Libraries.py
+++ Day26/External Libraries.py
@@ -1,3 +1,5 @@
+# Currency Converter using an external library
+
 from currency_converter import CurrencyConverter
 
 a = CurrencyConverter()
@@ -11,3 +13,21 @@ to_currency = input("Enter the to currency (e.g., INR): ").upper()      # conver
 conversion = a.convert(amt, from_currency, to_currency)
 
 print(f"Converted amount from {from_currency} to {to_currency} is: {conversion:.2f}")
+
+
+# Currency Converter with specifc date
+
+from currency_converter import CurrencyConverter
+from datetime import date  # Correct import for date
+
+a = CurrencyConverter()
+
+amt = float(input("Enter the amount: "))  # user input for amount
+
+from_currency = input("Enter the from currency (e.g., USD): ").upper()  # convert to uppercase
+to_currency = input("Enter the to currency (e.g., INR): ").upper()      # convert to uppercase
+
+# Use date from datetime module
+conversion = a.convert(amt, from_currency, to_currency, date=date(2020, 1, 3))
+
+print(f"Converted amount from {from_currency} to {to_currency} on 2020-01-01 is: {conversion:.3f}")
